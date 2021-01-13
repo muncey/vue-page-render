469d67011d15f73826354530b6ad840f8e8e38d6
muncey <philip.munce@gmail.com>
2021-01-13 22:07:24 +1100

    Initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,14 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/#configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  // mount: {},
+  // plugins: [],
+  // installOptions: {},
+  // devOptions: {},
+  // buildOptions: {},
+  alias: {
+    'vue': 'vue/dist/vue.esm-bundler.js'
+  }
+};
